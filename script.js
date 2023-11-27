ad7ff69cca59422c9e04167c50045213d1bcd0dc
alessiafabbioni <alessiafabbioni@gmail.com>
2023-11-27 17:00:21 +0100

    Check palindromo

---
+++ script.js
@@ -0,0 +1,19 @@
+// Creaimo la funzione per verificare se la parola è un palindroma
+
+function Palindromo (word) {
+    //Fai diventare la parola piccola
+    const cleanedWord = word.replace(/ /g, "").toLowerCase();
+    //Compara la parola con il suo inverso
+    return cleanedWord === cleanedWord.split("").reverse().join("");
+
+}
+
+
+//inserimento delle variabili
+const insertWord = prompt ("Inserisci una parola")
+if (Palindromo) {
+    console.log("La parola è un palindromo");
+} else {
+    console.log("La parola non è un palindromo")
+}
+
